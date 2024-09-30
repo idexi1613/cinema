ef6a881d0184b511135e37c506dbf7ac9825efe8
Irina Sukhomlinova <idexi1613@gmail.com>
2024-09-30 20:10:00 +0300

    Изменена структура проекта (удалены файлы и обновлены существующие)

--- config/settings.py
+++ config/settings.py
@@ -82,6 +82,20 @@ WSGI_APPLICATION = 'config.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.environ.get('DB_NAME'),
+        'USER': os.environ.get('DB_USER'),
+        'PASSWORD': os.environ.get('DB_PASSWORD'),
+        'HOST': os.environ.get('DB_HOST', '127.0.0.1'),
+        'PORT': os.environ.get('DB_PORT', 5432),
+        'OPTIONS': {
+            'options': '-c search_path=public,content'
+        }
+    }
+} 
+
 include(
     'components/database.py',
 )
